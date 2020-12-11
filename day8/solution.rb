5bc8c43d9fa758a004fdf97dbf66ceaff031669b
Decompiled <driftingeric@gmail.com>
2020-12-10 19:22:17 -0500

    solve day 8 part 2

--- day8/solution.rb
+++ day8/solution.rb
@@ -6,16 +6,37 @@ class Emulator
 		end
 	end
 	
+	def swap cmd
+		cmd == 'jmp' ? 'nop' : 'jmp'	
+	end
+
+	def can_swap cmd
+		cmd == 'jmp' || cmd == 'nop'
+	end
+	
 	def find_loop
+		last_swap = 0
+		while true do
 		acc = 0
 		sp = 0
 		seen = {}
-		while !seen[sp] do
+		swap = 0
+		swapped = false
+		puts 'trying', last_swap
+		while !seen[sp] && sp <= @rom.length do
 			seen[sp] = true
-			puts "sp", sp
-			pp @rom
+			return acc if sp >= 656
 			parts = @rom[sp].split(' ')
 			cmd = parts[0]
+			if !swapped && can_swap(cmd)
+				swap += 1
+				if swap > last_swap
+					puts "swaping", cmd
+					last_swap = swap
+					swapped = true
+					cmd = swap cmd.strip
+				end
+			end
 			val = parts[1].to_i
 			case cmd
 			when 'acc' 
@@ -29,8 +50,10 @@ class Emulator
 				puts "unknown command #{cmd}"
 			end
 		end
+		end
 		acc
 	end
+
 end
 
 puts Emulator.new.find_loop
